b1fea583c4c297f08897b6fad8ab7ac5b7d26bfa
clemdp <clement.dupuy1997@outlook.fr>
2020-12-22 23:01:53 +0100

    Chargement par défaut de la cell halfadder

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,12 +18,13 @@ using namespace Netlist;
 
 int main ( int argc, char* argv[] )
 {
+  Cell* halfadder = Cell::load("halfadder");
   std::cout << "[MAIN] : Creation of QApplication" << std::endl;
   QApplication* qa = new QApplication(argc, argv);
 
   std::cout << "[MAIN] : Creation of CellViewer" << std::endl;
   CellViewer* viewer = new CellViewer();
-  //viewer->setCell( halfadder );
+  viewer->setCell( halfadder );
   viewer->show();
 
   std::cout << "[MAIN] : Execution of QApplication" << std::endl;
