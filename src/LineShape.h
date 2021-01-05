d261894483e24795917fdc7919078bd0c8ec4ad5
clemdp <clement.dupuy1997@outlook.fr>
2021-01-05 18:35:21 +0100

    StarPoint et EndPoint (accesseurs)

--- src/LineShape.h
+++ src/LineShape.h
@@ -16,10 +16,15 @@ namespace Netlist
         public:
                                 LineShape         (Symbol * s, int x1, int y1, int x2, int y2);
                                 ~LineShape        ();
+            inline  Point       startPoint        () const;
+            inline  Point       endPoint          () const;
                     Box         getBoundingBox    () const;
             static  LineShape * fromXml           (Symbol *, xmlTextReaderPtr);
                     void        toXml             (std::ostream&);
     };
+
+    inline Point LineShape::startPoint() const { return Point(x1_, y1_); }
+    inline Point LineShape::endPoint  () const { return Point(x2_, y2_); }
 
 } // namespace Netlist
 #endif
